01c261f56c0b00bab554741cbbc55fda4d5074dc
Tad <tgetachew18@amherst.edu>
2017-11-14 05:32:14 -0500

    P2 done

--- inference.py
+++ inference.py
@@ -221,7 +221,13 @@ class ExactInference(InferenceModule):
         positions after a time update from a particular position.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        allPossible = util.Counter()
+        for oldPos in self.legalPositions:
+			newPosDist = self.getPositionDistribution(self.setGhostPosition(gameState, oldPos))
+			for newPos, prob in newPosDist.items():
+				allPossible[newPos] += self.beliefs[oldPos] * prob
+        allPossible.normalize()
+        self.beliefs = allPossible
 
     def getBeliefDistribution(self):
         return self.beliefs
